6029eaaf523c392e045a0eabbd8cc4061c290d00
don yegon <donyegon@gmail.com>
2015-10-28 11:38:04 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Yegon on 10/27/2015.
+ */
+var express = require("express");
+
+var app =express();
+
+var port = process.env.port || 3000;
+
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get(function(req,res){
+       var responseJson = {hello:"This is my api"};
+        res.json(responseJson);
+    });
+app.use('/api',bookRouter);
+
+
+
+app.get('/',function(req,res){
+    res.send('Welcome to my API');
+});
+
+app.listen(port,function(){
+   console.log('Gulp is running my app on PORT '+port);
+});
